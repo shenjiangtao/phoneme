6aae98c5073af020b3ca4afc12a41f8bec193f08
jiangtao.shen <jiangtao.shen@nio.com>
2025-04-10 11:06:00 +0800

    b08

--- feature/phoneme_feature-mr2-dev/midp/src/ams/jump_application/classes/com/sun/midp/jump/midletsuite/MIDletSuiteStorageAccessor.java
+++ feature/phoneme_feature-mr2-dev/midp/src/ams/jump_application/classes/com/sun/midp/jump/midletsuite/MIDletSuiteStorageAccessor.java
@@ -101,7 +101,7 @@ public class MIDletSuiteStorageAccessor
        }	   
    }
 
-    class JUMPMIDletSuiteInfo {
+   class JUMPMIDletSuiteInfo {
   
         MIDletSuiteInfo suiteInfo; 
         ArrayList midletApplications;
@@ -111,9 +111,8 @@ public class MIDletSuiteStorageAccessor
 
            midletApplications = new ArrayList(midletInfos.length);
            for (int i = 0; i < midletInfos.length; i++) {
-               MIDletApplication app = new MIDletApplication(midletInfos[i].classname,
-                          null, suiteInfo.suiteId);
-                          //midletInfos[i].icon, suiteInfo);
+               MIDletApplication app = new MIDletApplication(midletInfos[i].name,
+                          null, suiteInfo.suiteId, midletInfos[i].classname, (i+1)); 
                midletApplications.add(i, app);
            }
 
